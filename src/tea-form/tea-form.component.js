df616322ca08e520c0a36e3898fddc9d561a5bd7
Psalm Diao <p.diao25@gmail.com>
2025-09-09 22:16:37 +0200

    Implement tea form submit

--- src/tea-form/tea-form.component.js
+++ src/tea-form/tea-form.component.js
@@ -31,6 +31,10 @@ export class TeaFormComponent extends HTMLElement {
 
   connectedCallback() {
     window.routerLinkSetup(this.shadowRoot);
+    this.shadowRoot.querySelector('button#btn-submit').onclick = async (event) => {
+      event.preventDefault();
+      await this.submit();
+    };
   }
 
   fillTypeSelectOptions() {
@@ -71,11 +75,49 @@ export class TeaFormComponent extends HTMLElement {
   toggleCustomInputField(ref, custom) {
     const elems = this.shadowRoot.querySelectorAll(custom);
     this.shadowRoot.querySelector(ref).addEventListener('change', (event) => {
-      if ((event.target.value === '-' && elems[0].hasAttribute('hidden'))
-        || (event.target.value !== '-' && !elems[0].hasAttribute('hidden'))
+      if ((event.target.value === '' && elems[0].hasAttribute('hidden'))
+        || (event.target.value !== '' && !elems[0].hasAttribute('hidden'))
       ) {
-        elems.forEach(elem => elem.toggleAttribute('hidden'));
+        elems.forEach((elem) => {
+          elem.toggleAttribute('hidden'); // on by default
+          if (elem.querySelector('input').id === 'alias') return;
+          elem.querySelector('input').toggleAttribute('required'); // off by default
+        });
       }
     });
+  }
+
+  async submit() {
+    const form = this.shadowRoot.querySelector('form#create-tea-form');
+    const data = {
+      typeId: form.type.value,
+      nameId: form.nameId.value,
+      name: form.name.value,
+      alias: form.alias.value === '' ? null : form.alias.value,
+      harvestYear: form.harvestYear.value === '' ? null : form.harvestYear.value,
+      originId: form.originId.value,
+      origin: form.origin.value,
+      vendorId: form.vendorId.value,
+      vendor: form.vendor.value,
+      amount: form.amount.value,
+      isAvailable: form.isAvailable.checked,
+      remarks: form.remarks.value === '' ? null : form.remarks.value,
+    };
+    try {
+      const response = await fetch(`${API_ENDPOINT}/teas`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Response message: ${response.status}`);
+      }
+      const result = await response.json();
+      const id = result?.teaId;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
